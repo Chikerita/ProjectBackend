91ef3583f3b6ed9f030ed349ad64b27abefaff1b
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-11-09 10:42:05 -0500

    Microservicios

--- ServidorNode/app.js
+++ ServidorNode/app.js
@@ -1,27 +1,55 @@
+const mongo = require('mongodb');
 const express = require('express');
 const appServer = express();
 appServer.use(express.json());
 
-appServer.listen (3000, () => {
+var MongoClient = mongo.MongoClient;
+var url = "mongodb://localhost:27017";
+
+
+appServer.listen(3000, () => {
     console.log('SERVER IS RUNING IN PORT 3000');
 });
 
-appServer.get ('/get', 
-    (req, res) => {
-        res.send ('HOLA MUNDO');
-    }
-);
+appServer.get('/get', (req, res) => {
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db("mydb");
+        const options = {
+            sort: {},
+            projection: { _id: 0},
+        };
+        db.collection('CuestionariosPublicos').find({}, options).toArray(function (err, result){
+            if (err) throw err;
+            res.send(result);
+            client.close();
+        });
+    });
+});
 
-appServer.post ('/post', 
-    (req, res) => {
-        console.log(req.body);
-        res.send('POST Recibido');
-    }
-);
+appServer.put('/put', (req, res) => {
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db("mydb");
+        db.collection('CuestionariosPublicos').insertOne(req.body);
+        client.close();
+    });
+    res.send('Cuestionario recibido');
+});
 
-appServer.put ('/put', 
-    (req, res) => {
-        console.log(req.body);
-        res.send('PUT Recibido');
-    }
-);
+
+appServer.get('/getp', (req, res) => {
+    MongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db("mydb");
+        const options = {
+            sort: {},
+            projection: { _id: 0},
+        };
+        db.collection('CuestionariosPublicos').find({}, options).toArray(function (err, result){
+            if (err) throw err;
+            res.send(result);
+            client.close();
+        });
+    });
+});
