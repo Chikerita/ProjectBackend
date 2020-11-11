d97289ec45ec23050429e0cb7fa48941197ffc3f
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-11-11 17:20:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,52 @@
 const mongo = require('mongodb');
 const express = require('express');
 const appServer = express();
+const PORT = process.env.PORT || 3000;
+
 appServer.use(express.json());
 
-var MongoClient = mongo.MongoClient;
-var url = "mongodb://localhost:27017";
+var url = "mongodb+srv://admin:btCafd9cdd1d29fY@pruebadedespliegue.uwb3h.mongodb.net/mydb?retryWrites=true&w=majority";
+var MongoClient = new mongo.MongoClient(url);
 
-
-appServer.listen(3000, () => {
-    console.log('SERVER IS RUNING IN PORT 3000');
+appServer.listen(PORT, () => {
+    console.log("SERVER IS LISTEN ON PORT:" , PORT);
 });
 
-appServer.get('/gettest', (req, res) => {
-    res.send('Funciona');
+appServer.get('/get', async (req, res) => {
+    const results = await findQuestionarys(MongoClient);
+    res.send(results);
+    console.log("Respuesta enviada");
 });
 
-appServer.get('/get', (req, res) => {
-    MongoClient.connect(url, function (err, client) {
-        if (err) throw err;
-        var db = client.db("mydb");
+async function findQuestionarys(client) {
+    try {
+        await client.connect();
         const options = {
             sort: {},
             projection: { _id: 0},
         };
-        db.collection('CuestionariosPublicos').find({}, options).toArray(function (err, result){
-            if (err) throw err;
-            res.send(result);
-            client.close();
-        });
-    });
+        const cursor = client.db("mydb").collection('CuestionariosPublicos').find({}, options);
+        const results = await cursor.toArray();
+        return results;
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+appServer.put('/put', async (req, res) => {
+    addQuestionary(MongoClient, req.body);
+    res.send("Cuestionario agregado");
 });
 
-appServer.put('/put', (req, res) => {
-    MongoClient.connect(url, function (err, client) {
-        if (err) throw err;
-        var db = client.db("mydb");
-        db.collection('CuestionariosPublicos').insertOne(req.body);
-        client.close();
-    });
-    res.send('Cuestionario recibido');
-});
-
-
-appServer.get('/getp', (req, res) => {
-    MongoClient.connect(url, function (err, client) {
-        if (err) throw err;
-        var db = client.db("mydb");
-        const options = {
-            sort: {},
-            projection: { _id: 0},
-        };
-        db.collection('CuestionariosPublicos').find({}, options).toArray(function (err, result){
-            if (err) throw err;
-            res.send(result);
-            client.close();
-        });
-    });
-});
+async function addQuestionary(client, questionary){
+    try {
+        await client.connect();
+        const result = await client.db("mydb").collection('CuestionariosPublicos').insertOne(questionary);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
